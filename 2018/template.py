bc4bf5dee7a3f775be1aeb3e9708f929b1d7eb4f
Michael Matthews <iismichaels@gmail.com>
2018-12-04 00:15:46 -0500

    template file

---
+++ 2018/template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def processInput(fileName):
+    result = 0
+    with open(fileName) as f:
+        for line in f:
+
+    return result
+
+if __name__ == "__main__":
+    import sys
+    result = processInput(sys.argv[1])
+    print result
+    
